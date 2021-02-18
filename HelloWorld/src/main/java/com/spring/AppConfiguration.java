0a7c226d85799691767ea7c7bdf0df7ede9eca2f
DELL <DELL@DESKTOP-9C0R74H>
2021-02-18 09:02:27 +0530

    First commit

---
+++ HelloWorld/src/main/java/com/spring/AppConfiguration.java
@@ -0,0 +1,15 @@
+package com.spring;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppConfiguration {
+
+	
+	@RequestMapping("/hello")
+	public String hello()
+	{
+		return "Hello World!";
+	}
+}
